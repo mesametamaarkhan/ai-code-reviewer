8f68e847a7156ea7b866c5fc4417f38715e08620
Mesam E Tamaar Khan <mesamtamaark@gmail.com>
2026-06-21 14:05:08 +0500

    reviews now working with persistence

--- src/components/review-form.tsx
+++ src/components/review-form.tsx
@@ -56,6 +56,7 @@ export default function ReviewForm({
             body: JSON.stringify({
               code,
               language,
+              projectId,
             }),
           }
         );
